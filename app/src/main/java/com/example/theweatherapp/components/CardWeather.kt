2aabe0966a0734e3af00f5ece5beb034042ac5f3
João Pedro <joaobritosts@gmail.com>
2025-01-20 00:31:32 -0300

    Termino da criação da UI e implementação da response da API na tela

--- app/src/main/java/com/example/theweatherapp/components/CardWeather.kt
+++ app/src/main/java/com/example/theweatherapp/components/CardWeather.kt
@@ -1,13 +1,19 @@
 package com.example.theweatherapp.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,9 +21,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.theweatherapp.R
+import com.example.theweatherapp.ui.theme.DarkBlue
 
 @Composable
 fun CardWeather(label: String, value: String, icon: ImageVector) {
@@ -27,10 +38,35 @@ fun CardWeather(label: String, value: String, icon: ImageVector) {
         colors = CardDefaults.cardColors(Color.White),
         elevation = CardDefaults.cardElevation(6.dp)){
         Column(Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .padding(8.dp),
             horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.Top) {
-            Row {}
+            Row(verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start) {
+                Icon(imageVector = icon, contentDescription = null,
+                    Modifier.size(24.dp))
+                Spacer(Modifier.width(6.dp))
+                Text(text = label,
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        color = DarkBlue))
+            }
+            Spacer(Modifier.height(8.dp))
+
+            Box(Modifier.fillMaxWidth()
+                .weight(1f),
+                contentAlignment = Alignment.Center){
+
+                Text(text = value,
+                    style = TextStyle(
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        color = DarkBlue
+                    )
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/example/theweatherapp/presentation/WeatherScreen.kt
+++ app/src/main/java/com/example/theweatherapp/presentation/WeatherScreen.kt
@@ -2,25 +2,26 @@
 
 package com.example.theweatherapp.presentation
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -31,24 +32,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.theweatherapp.R
-import com.example.theweatherapp.ui.theme.DarkBlue
+import com.example.theweatherapp.components.CardWeather
 import com.example.theweatherapp.ui.theme.Pink40
-import com.example.theweatherapp.ui.theme.Purple40
-import com.example.theweatherapp.ui.theme.Purple80
-import com.example.theweatherapp.ui.theme.PurpleGrey40
-import com.example.theweatherapp.ui.theme.PurpleGrey80
 import com.example.theweatherapp.ui.theme.TheWeatherAppTheme
 import com.example.theweatherapp.view_model.WeatherViewModel
 
@@ -56,9 +54,7 @@ import com.example.theweatherapp.view_model.WeatherViewModel
 @Composable
 fun WeatherScreen(viewModel: WeatherViewModel) {
     val weatherData by viewModel.weatherData.collectAsState()
-    var city by remember {
-        mutableStateOf(value = "")
-    }
+    var city by remember {mutableStateOf(value = "")}
     val apiKey = "52f6a1b6489a878343b670e1be7174e5"
 
     Box(
@@ -66,9 +62,20 @@ fun WeatherScreen(viewModel: WeatherViewModel) {
             .fillMaxSize()
             .paint(
                 painterResource(id = R.drawable.weather_image2),
-                contentScale = ContentScale.FillBounds
-            )
+                contentScale = ContentScale.FillBounds)
     ) {
+
+        Text(text = "Weather App",
+            style = TextStyle(
+                fontSize = 52.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                color = Pink40),
+            modifier = Modifier
+                .fillMaxSize()
+                .fillMaxWidth()
+                .padding(start = 24.dp, top = 36.dp))
+
         Column(
             Modifier
                 .fillMaxSize()
@@ -112,7 +119,29 @@ fun WeatherScreen(viewModel: WeatherViewModel) {
                     )
                 )
             }
+            Spacer(Modifier.height(18.dp))
 
+            weatherData?.let {
+                Row (Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly){
+                    CardWeather(label = "City", value = it.name, icon = Icons.Default.Place)
+                    CardWeather(
+                        label = "Temperature", value = "${it.main.temp}",
+                        icon = Icons.Default.Star)
+                }
+                Spacer(Modifier.height(6.dp))
+
+                Row (Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Absolute.SpaceEvenly){
+                    CardWeather(
+                        label = "Humidity", value = "${it.main.humidity}%",
+                        icon = Icons.Default.Warning)
+                    CardWeather(
+                        label = "Description", value = it.weather[0].description,
+                        icon = Icons.Default.Info)
+
+                }
+            }
         }
     }
 }
